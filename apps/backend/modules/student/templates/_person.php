392a3ba2f04399b01cb62e8a7ff36abfc7dc9b8b
Maria Emilia Corrons <ecorrons@cespi.unlp.edu.ar>
2013-07-12 17:49:27 -0300

    Cambios en template de Ver Detalle de alumnos

--- apps/backend/modules/student/templates/_person.php
+++ apps/backend/modules/student/templates/_person.php
@@ -1,5 +1,4 @@
-<?php 
-/*
+<?php /*
  * Kimkëlen - School Management Software
  * Copyright (C) 2013 CeSPI - UNLP <desarrollo@cespi.unlp.edu.ar>
  *
@@ -18,88 +17,77 @@
  * along with Kimkëlen.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
  */ ?>
 <h2><?php echo __('Personal data') ?> </h2>
-  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_full_name">
+<div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_full_name">
+  <div>
+    <label for="full_name"> <?php echo ('Nombre completo'); ?> </label>
+    <?php echo $student->getPerson()->getFullName(); ?>
+  </div>
+  <div style="margin-top: 1px; clear: both;"></div>
+</div>
+<div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_identification_type_and_number">
+  <div>
+    <label for="identification_type_and_number"> <?php echo __('Tipo y número de documento'); ?> </label>
+    <?php echo BaseCustomOptionsHolder::getInstance('IdentificationType')->getStringFor($student->getPerson()->getIdentificationType()) . ': ' . $student->getPerson()->getIdentificationNumber(); ?>
+  </div>
+  <div style="margin-top: 1px; clear: both;"></div>
+</div>
+<div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_sex_type">
+  <div>
+    <label for="sex_type"> <?php echo __('Sexo'); ?> </label>
+    <?php echo BaseCustomOptionsHolder::getInstance('SexType')->getStringFor($student->getPerson()->getSex()); ?>
+  </div>
+  <div style="margin-top: 1px; clear: both;"></div>
+</div>
+<?php if (!$sf_user->hasCredential('show_student_min')): ?>
+  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_email">
     <div>
-    <label for="full_name"> <?php echo ('Nombre completo');?> </label>
-      <?php echo $student->getPerson()->getFullName();?>
+      <label for="email"> <?php echo __('Email'); ?> </label>
+      <?php echo (is_null($student->getPerson()->getEmail()) | $student->getPerson()->getEmail() == '') ? 'No ha sido cargado' : $student->getPerson()->getEmail(); ?>
     </div>
     <div style="margin-top: 1px; clear: both;"></div>
   </div>
-  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_identification_type_and_number">
+  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_telefono">
     <div>
-    <label for="identification_type_and_number"> <?php echo __('Tipo y número de documento');?> </label>
-      <?php echo BaseCustomOptionsHolder::getInstance('IdentificationType')->getStringFor($student->getPerson()->getIdentificationType()).': '.$student->getPerson()->getIdentificationNumber();?>
+      <label for="telefono"> <?php echo __('Teléfono'); ?> </label>
+      <?php echo (is_null($student->getPerson()->getPhone()) | $student->getPerson()->getPhone() == '') ? 'No ha sido cargado' : $student->getPerson()->getPhone(); ?>
     </div>
     <div style="margin-top: 1px; clear: both;"></div>
   </div>
-  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_sex_type">
-    <div>
-    <label for="sex_type"> <?php echo __('Sexo');?> </label>
-      <?php echo BaseCustomOptionsHolder::getInstance('SexType')->getStringFor($student->getPerson()->getSex());?>
-    </div>
-    <div style="margin-top: 1px; clear: both;"></div>
+<?php endif ?>
+<div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_birth_date">
+  <div>
+    <label for="birth_date"> <?php echo __('Fecha de nacimiento'); ?> </label>
+    <?php echo $student->getPerson()->getBirthdate('d-m-Y'); ?>
   </div>
-  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_cuil">
-    <div>
-    <label for="cuil"> <?php echo __('Cuil');?> </label>
-      <?php echo (is_null($student->getPerson()->getCuil()) | $student->getPerson()->getCuil() == '') ? 'No ha sido cargado' : $student->getPerson()->getCuil();?>
-    </div>
-    <div style="margin-top: 1px; clear: both;"></div>
-  </div>
-  <?php if (!$sf_user->hasCredential('show_student_min')): ?>
-    <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_email">
-      <div>
-        <label for="email"> <?php echo __('Email');?> </label>
-        <?php echo (is_null($student->getPerson()->getEmail()) | $student->getPerson()->getEmail() == '') ? 'No ha sido cargado' : $student->getPerson()->getEmail();?>
-      </div>
-      <div style="margin-top: 1px; clear: both;"></div>
-    </div>
-    <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_telefono">
-      <div>
-        <label for="telefono"> <?php echo __('Teléfono');?> </label>
-        <?php echo (is_null($student->getPerson()->getPhone()) | $student->getPerson()->getPhone() == '') ? 'No ha sido cargado' : $student->getPerson()->getPhone();?>
-      </div>
-      <div style="margin-top: 1px; clear: both;"></div>
-    </div>
-  <?php endif ?>
-  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_birth_date">
-    <div>
-    <label for="birth_date"> <?php echo __('Fecha de nacimiento');?> </label>
-      <?php echo $student->getPerson()->getBirthdate('d-m-Y');?>
-    </div>
-    <div style="margin-top: 1px; clear: both;"></div>
-  </div>
-  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_birth_country">
-    <div>
-    <label for="birth_country"> <?php echo __('País de nacimiento');?> </label>
-      <?php echo (is_null($student->getPerson()->getBirthCountry()) | $student->getPerson()->getBirthCountry() == '') ? 'No ha sido cargado' : $student->getPerson()->getBirthCountryRepresentation();?>    </div>
-    <div style="margin-top: 1px; clear: both;"></div>
-  </div>
-  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_birth_state">
-    <div>
-    <label for="birth_state"> <?php echo __('Provincia de nacimiento');?> </label>
-      <?php echo (is_null($student->getPerson()->getBirthState()) | $student->getPerson()->getBirthState() == '') ? 'No ha sido cargado' : $student->getPerson()->getBirthStateRepresentation();?>    </div>
-    <div style="margin-top: 1px; clear: both;"></div>
-  </div>
-  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_birth_city">
-    <div>
-    <label for="birth_city"> <?php echo __('Ciudad de nacimiento');?> </label>
-      <?php echo (is_null($student->getPerson()->getBirthCity()) | $student->getPerson()->getBirthCity() == '') ? 'No ha sido cargada' : $student->getPerson()->getBirthCityRepresentation();?>    </div>
-    <div style="margin-top: 1px; clear: both;"></div>
-  </div>
+  <div style="margin-top: 1px; clear: both;"></div>
+</div>
+<div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_birth_country">
+  <div>
+    <label for="birth_country"> <?php echo __('Lugar de nacimiento'); ?> </label>
+    <?php echo (is_null($student->getPerson()->getBirthCity()) | $student->getPerson()->getBirthCity() == '') ? 'No ha sido cargada' : $student->getPerson()->getBirthCityRepresentation(); ?>
+    , <?php echo (is_null($student->getPerson()->getBirthState()) | $student->getPerson()->getBirthState() == '') ? 'No ha sido cargado' : $student->getPerson()->getBirthStateRepresentation(); ?>
+    , <?php echo (is_null($student->getPerson()->getBirthCountry()) | $student->getPerson()->getBirthCountry() == '') ? 'No ha sido cargado' : $student->getPerson()->getBirthCountryRepresentation(); ?></div>
+  <div style="margin-top: 1px; clear: both;"></div>
+</div>
+<div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_address">
+  <div>
+    <label for="address"> <?php echo __('Address'); ?> </label>
+    <?php echo (is_null($student->getPerson()->getAddress()) | $student->getPerson()->getAddress() == '') ? 'No ha sido cargado' : $student->getPerson()->getAddress(); ?>    </div>
+  <div style="margin-top: 1px; clear: both;"></div>
+</div>
 
-  <?php  include_partial('personal/person_photo', array('object' => $student)); ?>
+<?php include_partial('personal/person_photo', array('object' => $student)); ?>
 
-  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_observations">
-    <div>
-    <label for="observations"> <?php echo __('Observaciones');?> </label>
-      <?php echo (is_null($student->getPerson()->getObservations()) | $student->getPerson()->getObservations() == '') ? 'No han sido cargadas' : $student->getPerson()->getObservations();?>    </div>
-    <div style="margin-top: 1px; clear: both;"></div>
-  </div>
+<div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_observations">
+  <div>
+    <label for="observations"> <?php echo __('Observaciones'); ?> </label>
+    <?php echo (is_null($student->getPerson()->getObservations()) | $student->getPerson()->getObservations() == '') ? 'No han sido cargadas' : $student->getPerson()->getObservations(); ?>    </div>
+  <div style="margin-top: 1px; clear: both;"></div>
+</div>
 <h2><?php echo __('System access') ?> </h2>
-  <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_username">
-    <div>
-    <label for="username"> <?php echo __('Nombre de usuario');?> </label>
-      <?php echo (is_null($student->getPerson()->getsfGuardUser())) ? 'No posee usuario' : $student->getPerson()->getsfGuardUser()->getUsername();?>    </div>
-    <div style="margin-top: 1px; clear: both;"></div>
-  </div>
+<div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_username">
+  <div>
+    <label for="username"> <?php echo __('Nombre de usuario'); ?> </label>
+    <?php echo (is_null($student->getPerson()->getsfGuardUser())) ? 'No posee usuario' : $student->getPerson()->getsfGuardUser()->getUsername(); ?>    </div>
+  <div style="margin-top: 1px; clear: both;"></div>
+</div>
