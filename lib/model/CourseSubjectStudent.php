5b9098419b698714da00c03b0bccff5c41ccae6d
María Emilia Corrons <ecorrons@cespi.unlp.edu.ar>
2013-09-27 14:35:02 -0300

    Modif para que salga bien la advertencia de si esta libre en form de calificaciones

--- lib/model/CourseSubjectStudent.php
+++ lib/model/CourseSubjectStudent.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
  * Kimkëlen - School Management Software
  * Copyright (C) 2013 CeSPI - UNLP <desarrollo@cespi.unlp.edu.ar>
@@ -396,10 +396,10 @@ class CourseSubjectStudent extends BaseCourseSubjectStudent
     {
       $c->add(StudentFreePeer::COURSE_SUBJECT_ID, $this->getCourseSubjectId());
     }
-    else
-    {
-      $c->add(StudentFreePeer::COURSE_SUBJECT_ID, null, Criteria::ISNULL);
-    }
+    //else
+    //{
+      //$c->add(StudentFreePeer::COURSE_SUBJECT_ID, null, Criteria::ISNULL);
+    //}
 
     return StudentFreePeer::doSelect($c);
 
