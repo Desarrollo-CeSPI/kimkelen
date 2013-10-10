c60c76248ce0ef731e6e21a6b3656564ac6b19ff
María Emilia Corrons <ecorrons@cespi.unlp.edu.ar>
2013-10-10 16:28:23 -0300

    Planilla de calificaciones

--- apps/backend/modules/course_student_mark/templates/_course_subject_marks_table.php
+++ apps/backend/modules/course_student_mark/templates/_course_subject_marks_table.php
@@ -1,5 +1,4 @@
-<?php 
-/*
+<?php /*
  * Kimkëlen - School Management Software
  * Copyright (C) 2013 CeSPI - UNLP <desarrollo@cespi.unlp.edu.ar>
  *
@@ -17,47 +16,88 @@
  * You should have received a copy of the GNU General Public License
  * along with Kimkëlen.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
  */ ?>
-<?php use_stylesheet('print_table.css', 'last', array('media' => 'print')) ?>
+<?php use_stylesheet('report-card.css', 'first', array('media' => 'screen')) ?>
+<?php use_stylesheet('print-report-card.css', 'last', array('media' => 'print')) ?>
+<?php use_stylesheet('main.css', '', array('media' => 'all')) ?>
 
-<?php foreach ($course_subjects as $course_subject): ?>
-  <div class="print_page">
+<div class="non-printable">
+  <span><a href="#" onclick="window.print(); return false;"><?php echo __('Print') ?></a></span>
+  <span><a href="<?php echo url_for('course_student_mark/goBack') ?>"><?php echo __('Go back') ?></a></span>
+  <form action="<?php echo url_for('@print_table') ?>" method="post" target="_blank" id="exportation_form">
+    <p><?php echo __('Export to excel') ?> <?php echo image_tag('export_to_excel.gif', array('class' => 'excel_button')) ?></p>
+    <input type="hidden" id="send_data" name="send_data" />
+  </form>
+</div>
+
+<div class="report-wrapper"  id="export_to_excel">
+  <?php foreach ($course_subjects as $course_subject): ?>
     <?php $career = $course_subject->getCareerSubjectSchoolYear()->getCareerSchoolYear()->getCareer(); ?>
     <?php $course = $course_subject->getCourse(); ?>
     <?php $final_period = $course_subject->isFinalPeriod(); ?>
     <?php $configuration = $course_subject->getCareerSubjectSchoolYear()->getConfiguration() ?>
 
-    <div class="sf_admin_form_row sf_admin_Text sf_admin_form_field_students">
-      <div>
-        <table  width="100%" border="0" cellspacing="1" cellpadding="2" class="tbl_content" id="export_to_excel">
-          <thead>
-            <?php include_partial("course_student_mark/thead",array('course' => $course, "configuration" => $configuration));?>
-          </thead>          
-
-          <tbody class="print_body">
-            <?php include_partial("course_student_mark/tbody", array('course' => $course, "configuration" => $configuration, "course_subject" => $course_subject, "final_period" => $final_period));?>
-            <?php include_partial("course_student_mark/tfoot", array('configuration' => $configuration, "course_subject" => $course_subject));?>
-          </tbody>
-          
-        </table>
-        </div>
-      <div style="margin-top: 1px; clear: both;"></div>
+    <div class="report-header">
+      <div class="logo"><?php echo image_tag("kimkelen_logo.png", array('absolute' => true)) ?></div>
+      <div class="header_row">
+        <h2><?php echo __('Print califications'); ?></h2>
+        <div class="title"><?php echo __('School year') ?>: </div>
+        <div class="orientation"><?php echo $course->getSchoolYear() ?></div>
+        <div class="title"><?php echo __('Año/Nivel') ?>: </div>
+        <div class="course"><?php echo $course->getYear() ?></div>
+        <?php if (!(is_null($course->getDivision()))): ?>
+          <div class="title"><?php echo __('Division') ?>: </div>
+          <div class="course"><?php echo $course->getDivision()->getDivisionTitle(); ?></div>
+        <?php endif; ?>
+        <div class="title"><?php echo __('Subject') ?>: </div>
+        <div class="orientation"><?php echo $course->getSubjectsStr(); ?></div>
+      </div>
+      <div class="header_row">
+        <div class="title"><?php echo __('Teacher') ?>: </div>
+        <div class="orientation"><?php echo $course->getTeachersStr() ?></div>
+      </div>
     </div>
-  </div>
-<?php endforeach ?>
 
-<form action="<?php echo url_for('@print_table')?>" method="post" target="_blank" id="exportation_form">
-  <p><?php echo __('Export to excel') ?> <?php echo image_tag('export_to_excel.gif', array('class' => 'excel_button')) ?></p>
-  <input type="hidden" id="send_data" name="send_data" />
-  <a href="<?php echo url_for('course_student_mark/goBack') ?>"><?php echo __('Go back') ?></a>
-</form>
+    <div style="clear:both"></div>
+    <table width="100%" class="gridtable_bordered">
+      <?php include_partial("course_student_mark/thead", array('course' => $course, "configuration" => $configuration)); ?>
+      <tbody class="print_body">
+        <?php include_partial("course_student_mark/tbody", array('course' => $course, "configuration" => $configuration, "course_subject" => $course_subject, "final_period" => $final_period)); ?>
+      </tbody>
+    </table>
+  </div>
+  <div class="report-wrapper">
+    <?php foreach (range(1, $configuration->getCourseMarks()) as $number): ?>
+      <div class="teacher_signature_box">
+        <div class="titletable">
+          <span><?php echo __('Término ') . $number ?></span></div>
+        <div style="margin-top: 16%">
+          <div align="center">_________________________</div>
+          <div align="center"><?php echo __('Professor signature') ?></div>
+          <br>
+          <div align="center"><?php echo __('Fecha') ?> _____ / _____ / _____ </div>
+        </div>
+      </div>
+
+    <?php endforeach; ?>
+  </div>
+<?php endforeach; ?>
+<div style="clear:both"></div>
+<div class="non-printable">
+  <span><a href="#" onclick="window.print(); return false;"><?php echo __('Print') ?></a></span>
+  <span><a href="<?php echo url_for('course_student_mark/goBack') ?>"><?php echo __('Go back') ?></a></span>
+  <form action="<?php echo url_for('@print_table') ?>" method="post" target="_blank" id="exportation_form">
+    <p><?php echo __('Export to excel') ?> <?php echo image_tag('export_to_excel.gif', array('class' => 'excel_button')) ?></p>
+    <input type="hidden" id="send_data" name="send_data" />
+  </form>
+</div>
 
 <script language="javascript">
-jQuery(document).ready(function()
-{
-  jQuery(".excel_button").click(function(event)
+  jQuery(document).ready(function()
   {
-    jQuery("#send_data").val( jQuery("<div>").append( jQuery("#export_to_excel").eq(0).clone()).html());
-    jQuery("#exportation_form").submit();
+    jQuery(".excel_button").click(function(event)
+    {
+      jQuery("#send_data").val( jQuery("<div>").append( jQuery("#export_to_excel").eq(0).clone()).html());
+      jQuery("#exportation_form").submit();
+    });
   });
-});
 </script>
