80dba4ff0d825a033c8d8e28db9a13149e247228
Juan Pablo <jpablop@cespi.unlp.edu.ar>
2013-11-15 17:30:22 -0300

    Se agrega para flavor BBA que se muestren los changlos en las notas x ajax

--- flavors/bba/modules/course_student_mark/templates/_mark.php
+++ flavors/bba/modules/course_student_mark/templates/_mark.php
@@ -17,6 +17,8 @@
  * You should have received a copy of the GNU General Public License
  * along with Kimkëlen.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
  */ ?>
+<?php use_helper('Javascript', 'Object','I18N','Form', 'Asset') ?>
+
 <?php $subject_configuration = $course_subject_student->getCourseSubject()->getCareerSubjectSchoolYear()->getConfiguration(); ?>
 <?php foreach($marks as $mark): ?>
   <?php $field = $form[$course_subject_student->getId().'_'.$mark->getMarkNumber()]; ?>
@@ -39,6 +41,22 @@
 
     <?php endif?>
 
-    <?php echo $mark->renderChangelog(); ?>
+      <a id="course_subject_student_mark_change_log_<?php echo $mark->getId() ?>" style="display: none;"> </a>
+      <?php
+      echo link_to_function( "<img style='vertical-align: middle;' src='".image_path('clock.png')."' alt='Historial de cambios' title='Ver historial'>" , "jQuery.ajax({
+      url: '" . url_for("@course_subject_student_mark_change_log?id=" . $mark->getId()) . "',
+      success: function (data)
+      {
+        var element = jQuery('#course_subject_student_mark_change_log_" . $mark->getId() ."');
+        element.html(data);
+        element.show();
+        jQuery('#course_subject_student_mark_" . $mark->getId() . "_ajax_show').hide();
+
+        jQuery('.nc_changelog_tooltip').fadeOut(500);
+        jQuery('#changelog_for_mark_". $mark->getId() .":hidden').fadeIn(500);
+      }
+    });", array('id' => "course_subject_student_mark_" . $mark->getId() . "_ajax_show", 'title' => __('show change log'),'display'=> 'inline-block'))
+
+      ?>
   </div>
 <?php endforeach; ?>
