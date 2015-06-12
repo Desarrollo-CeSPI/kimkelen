11fb61b4f2e539c4fc1feefdd6a86c3db82eefdd
María Emilia Corrons <ecorrons@cespi.unlp.edu.ar>
2015-06-12 11:04:15 -0300

    changes

--- lib/model/CourseSubjectStudentPathway.php
+++ lib/model/CourseSubjectStudentPathway.php
@@ -12,28 +12,5 @@ class CourseSubjectStudentPathway extends BaseCourseSubjectStudentPathway
 
   }
 
-	protected function doSave(PropelPDO $con)
-	{
-		try
-		{
-			$con->beginTransaction();
-			if ($this->countCourseSubjectStudentPathwayMarks() == 0)
-			{
-				for ($i = 1; $i <= $this->getCourseSubject()->getCareerSubjectSchoolYear()->getConfiguration()->getCourseMarks(); $i++)
-				{
-					$course_subject_student_mark = new CourseSubjectStudentPathwayMark();
-					$course_subject_student_mark->setCourseSubjectStudentPathway($this);
-					$course_subject_student_mark->setMarkNumber($i);
-					$course_subject_student_mark->save($con);
-				}
-			}
-			parent::doSave($con);
-			$con->commit();
-		}
-		catch (PropelException $e)
-		{
-			$con->rollBack();
-		}
 
-	}
 }
