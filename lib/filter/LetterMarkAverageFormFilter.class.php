09ddbd8f9e1d3ef04f2ec78dbfd60a7e4cde82dc
Ezequiel Gonzalez <el.palme@gmail.com>
2015-12-21 12:00:59 -0300

    patch y schema  con tabla de promedio en notas

---
+++ lib/filter/LetterMarkAverageFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * LetterMarkAverage filter form.
+ *
+ * @package    symfony
+ * @subpackage filter
+ * @author     Your name here
+ */
+class LetterMarkAverageFormFilter extends BaseLetterMarkAverageFormFilter
+{
+  public function configure()
+  {
+  }
+}
