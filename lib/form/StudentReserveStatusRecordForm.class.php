25c721656df2469495c8a4e8e2000b510f070f24
Naimid <nai.videla@gmail.com>
2016-06-24 12:56:06 -0300

    cambios

---
+++ lib/form/StudentReserveStatusRecordForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * StudentReserveStatusRecord form.
+ *
+ * @package    symfony
+ * @subpackage form
+ * @author     Your name here
+ */
+class StudentReserveStatusRecordForm extends BaseStudentReserveStatusRecordForm
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/model/StudentReserveStatusRecordPeer.php
@@ -0,0 +1,5 @@
+<?php
+
+class StudentReserveStatusRecordPeer extends BaseStudentReserveStatusRecordPeer
+{
+}
