e4b1cd075d04d0e00138741d2cda6feb47698e65
Ezequiel Gonzalez <egonzalez@cespi.unlp.edu.ar>
2016-07-07 10:56:43 -0300

    reestructura de filtro de docentes, se quitan filtros y se agregan otros

--- lib/filter/TeacherFormFilter.class.php
+++ lib/filter/TeacherFormFilter.class.php
@@ -14,7 +14,7 @@ class TeacherFormFilter extends BaseTeacherFormFilter
   public function removeFields()
   {
     unset(
-      $this['final_examination_subject_teacher_list'], $this['examination_subject_teacher_list'], $this['examination_repproved_subject_teacher_list'], $this['person_id']
+      $this['final_examination_subject_teacher_list'], $this['examination_subject_teacher_list'], $this['examination_repproved_subject_teacher_list'], $this['person_id'], $this['salary'], $this['aging_institution'], $this['file_number'] 
     );
 
   }
@@ -24,77 +24,53 @@ class TeacherFormFilter extends BaseTeacherFormFilter
     $this->removeFields();
 
     //widgets
-    $this->setWidget('person', new sfWidgetFormInput());
-    $this->setValidator('person', new sfValidatorString(array('required' => false)));
+    $this->setWidget('lastname', new sfWidgetFormInput());
+    $this->setValidator('lastname', new sfValidatorString(array('required' => false)));
+
+    $this->setWidget('firstname', new sfWidgetFormInput());
+    $this->setValidator('firstname', new sfValidatorString(array('required' => false)));
+
+    $this->setWidget('identification_number', new sfWidgetFormInput());
+    $this->setValidator('identification_number', new sfValidatorNumber(array('required' => false)));
 
     $this->setWidget('is_active', new sfWidgetFormChoice(array('choices' => array('' => '', 1 => 'Sí', 0 => 'No'))));
     $this->setValidator('is_active', new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))));
 
-    $this->setWidget('career_school_year_id', new sfWidgetFormPropelChoice(array('model' => 'CareerSchoolYear', 'add_empty' => true)));
-    $this->setValidator('career_school_year_id', new sfValidatorPropelChoice(array('model' => 'CareerSchoolYear', 'required' => false)));
-
-    $w = new sfWidgetFormChoice(array('choices' => array()));
-    $this->setWidget('year', new dcWidgetAjaxDependence(array(
-        'dependant_widget' => $w,
-        'observe_widget_id' => 'teacher_filters_career_school_year_id',
-        "message_with_no_value" => "Seleccione una carrera y apareceran los años que correspondan",
-        'get_observed_value_callback' => array(get_class($this), 'getYears')
-      )));
-    $this->setValidator('year', new sfValidatorInteger(array('required' => false)));
-
-    $courses_choices = new sfWidgetFormPropelChoice(array('model' => 'CareerSubjectSchoolYear', 'add_empty' => true));
-
-    $this->setWidget('courses', new dcWidgetAjaxDependence(array(
-        'dependant_widget' => $courses_choices,
-        'observe_widget_id' => 'teacher_filters_year',
-        "message_with_no_value" => "Seleccione una carrera y un año",
-        'get_observed_value_callback' => array(get_class($this), 'getCourses')
-      )));
-    $this->setValidator('courses', new sfValidatorPass());
-
-
-    $this->setWidget('subject', new sfWidgetFormPropelChoice(array('model' => 'Subject', 'add_empty' => true)));
+    $this->setWidget('subject', new sfWidgetFormPropelChoice(array('model' => 'Subject', 'add_empty' => true, 'order_by' => array('Name', 'asc'))));
     $this->setValidator('subject', new sfValidatorInteger(array('required'=>false)));
 
-
     //widgets options
-    $this->getWidgetSchema()->setLabel('person', 'Que contenga el nombre');
-    $this->getWidgetSchema()->setHelp('person', 'Se filtrarán todas las docentes que contengan lo ingresado en el nombre o apellido o numero de documento');
+    $this->getWidgetSchema()->setLabel('identification_number', 'Documento');
+    $this->getWidgetSchema()->setHelp('lastname', 'Se filtrara por apellido del docente.');
+    $this->getWidgetSchema()->setHelp('firstname', 'Se filtrara por nombre del docente.');
+    $this->getWidgetSchema()->setHelp('identification_number', 'Se filtrara por numero de documento del docente.');
+    $this->getWidgetSchema()->setHelp('subject', 'Se filtrara por materia que dicta el docente.');
 
     $this->validatorSchema->setOption('allow_extra_fields', true);
 
   }
 
-  public static function getYears($widget, $values)
-  {
-    $career = CareerSchoolYearPeer::retrieveByPk($values)->getCareer();
-    $choices = $career->getYearsForOption(true);
-    $widget->setOption('choices', $choices);
-    sfContext::getInstance()->getUser()->setAttribute('career_school_year_id', $values);
-
-  }
-
-  public static function getCourses($widget, $values)
-  {
-    $career_school_year_id = sfContext::getInstance()->getUser()->getAttribute('career_school_year_id');
-    $criteria = new Criteria();
-    $criteria->add(CareerSubjectPeer::YEAR, $values);
-    $criteria->add(CareerSubjectPeer::IS_OPTION, false);
-    $criteria->addJoin(CareerSubjectPeer::ID, CareerSubjectSchoolYearPeer::CAREER_SUBJECT_ID);
-    $criteria->add(CareerSubjectSchoolYearPeer::CAREER_SCHOOL_YEAR_ID, $career_school_year_id);
-    $criteria->addJoin(CareerSubjectPeer::SUBJECT_ID, SubjectPeer::ID);
-    $criteria->addAscendingOrderByColumn(SubjectPeer::NAME);
-    $widget->setOption('criteria', $criteria);
-
-  }
-
   public function getFields()
   {
-    return array_merge(parent::getFields(), array('person' => 'Text', 'career_school_year_id' => 'Number', 'year' => 'Number', 'courses' => 'Number', 'subject' => 'Number'));
+    return array_merge(parent::getFields(), array('lastname' => 'Text', 'firstname' => 'Text', 'identification_number' => 'Text', 'is_active' => 'Boolean', 'subject' => 'Number'));
 
   }
 
-  public function addPersonColumnCriteria(Criteria $criteria, $field, $value)
+  public function addLastnameColumnCriteria(Criteria $criteria, $field, $value)
+  {
+    $value = trim($value);
+    if ($value != '')
+    {
+      $value = "%$value%";
+      $criteria->setIgnoreCase(true);
+      $criteria->addJoin(TeacherPeer::PERSON_ID, PersonPeer::ID);
+      $criterion = $criteria->getNewCriterion(PersonPeer::LASTNAME, $value, Criteria::LIKE);
+      $criteria->add($criterion);
+    }
+
+  }
+
+  public function addFirstnameColumnCriteria(Criteria $criteria, $field, $value)
   {
     $value = trim($value);
     if ($value != '')
@@ -103,32 +79,46 @@ class TeacherFormFilter extends BaseTeacherFormFilter
       $criteria->setIgnoreCase(true);
       $criteria->addJoin(TeacherPeer::PERSON_ID, PersonPeer::ID);
       $criterion = $criteria->getNewCriterion(PersonPeer::FIRSTNAME, $value, Criteria::LIKE);
-      $criterion->addOr($criteria->getNewCriterion(PersonPeer::LASTNAME, $value, Criteria::LIKE));
-      $criterion->addOr($criteria->getNewCriterion(PersonPeer::IDENTIFICATION_NUMBER, $value, Criteria::LIKE));
       $criteria->add($criterion);
     }
 
   }
 
-  public function addCareerSchoolYearIdColumnCriteria(Criteria $criteria, $field, $value)
+  public function addIdentificationNumberColumnCriteria(Criteria $criteria, $field, $value)
   {
-    $criteria->addJoin(CourseSubjectTeacherPeer::TEACHER_ID, TeacherPeer::ID);
-    $criteria->addJoin(CourseSubjectPeer::ID, CourseSubjectTeacherPeer::COURSE_SUBJECT_ID);
-    $criteria->addJoin(CourseSubjectPeer::CAREER_SUBJECT_SCHOOL_YEAR_ID, CareerSubjectSchoolYearPeer::ID);
-    $criteria->add(CareerSubjectSchoolYearPeer::CAREER_SCHOOL_YEAR_ID, $value);
+    $value = trim($value);
+    if ($value != '')
+    {
+      $value = "%$value%";
+      $criteria->addJoin(TeacherPeer::PERSON_ID, PersonPeer::ID);
+      $criterion = $criteria->getNewCriterion(PersonPeer::IDENTIFICATION_NUMBER, $value, Criteria::LIKE);
+      $criteria->add($criterion);
+      $criteria->setDistinct();
+    }
 
   }
 
-  public function addYearColumnCriteria(Criteria $criteria, $field, $value)
+  public function addIsActiveColumnCriteria(Criteria $criteria, $field, $value)
   {
-    $criteria->add(CareerSubjectPeer::YEAR, $value);
-    $criteria->addJoin(CareerSubjectPeer::ID, CareerSubjectSchoolYearPeer::CAREER_SUBJECT_ID);
+    if ($value != '')
+    {
+      $c = new Criteria();
+      $c->addJoin(TeacherPeer::PERSON_ID, PersonPeer::ID);
+      $c->add(PersonPeer::IS_ACTIVE, 1);
+      $c->clearSelectColumns();
+      $c->addSelectColumn(PersonPeer::ID);
+      $stmt = PersonPeer::doSelectStmt($c);
+      $ids = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
-  }
-
-  public function addCoursesColumnCriteria(Criteria $criteria, $field, $value)
-  {
-    $criteria->add(CourseSubjectPeer::CAREER_SUBJECT_SCHOOL_YEAR_ID, $value);
+      if($value == '1')
+      {
+        $criteria->add(TeacherPeer::PERSON_ID, $ids, Criteria::IN);
+      }
+      else
+      {
+        $criteria->add(TeacherPeer::PERSON_ID, $ids, Criteria::NOT_IN);
+      }
+    }
 
   }
 
