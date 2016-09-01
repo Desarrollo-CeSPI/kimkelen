c26423b10b9e2b62c1d6de15dd7c453c24cfb3a3
Naimid <nai.videla@gmail.com>
2016-09-01 14:15:59 -0300

    modificacion en lettermarkaverage

--- lib/model/LetterMarkAveragePeer.php
+++ lib/model/LetterMarkAveragePeer.php
@@ -14,18 +14,18 @@ class LetterMarkAveragePeer extends BaseLetterMarkAveragePeer
   	$criteria = new Criteria();
   	
   	 
-    if (array_key_exists(1, $marks) && !is_null($mark[1])) {
+    if (array_key_exists(1, $marks) && ! is_null($marks[1])) {
   	  $criteria->add(LetterMarkAveragePeer::LETTER_MARK_1, $marks[1]->getId());
     } else {
       $criteria->add(LetterMarkAveragePeer::LETTER_MARK_1, null, Criteria::ISNULL);
     }
 
-    if (array_key_exists(2, $marks) && !is_null($mark[2])) {
+    if (array_key_exists(2, $marks) && !is_null($marks[2])) {
   	  $criteria->add(LetterMarkAveragePeer::LETTER_MARK_2, $marks[2]->getId());
     } else {
       $criteria->add(LetterMarkAveragePeer::LETTER_MARK_2, null, Criteria::ISNULL);
     }
-  	   if (array_key_exists(3, $marks) && !is_null($mark[3])) {
+  	   if (array_key_exists(3, $marks) && !is_null($marks[3])) {
       $criteria->add(LetterMarkAveragePeer::LETTER_MARK_3, $marks[3]->getId());
     } else {
       $criteria->add(LetterMarkAveragePeer::LETTER_MARK_3, null, Criteria::ISNULL);
