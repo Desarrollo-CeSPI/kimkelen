7fd55578e192838b671b7056387d2735528ea004
Ezequiel Gonzalez <egonzalez@cespi.unlp.edu.ar>
2016-09-05 12:10:40 -0300

    agrego la seleccion de departamento en formularios de jefe de preceptores y personal de oficina de alumnos

--- lib/form/custom/HeadPersonalForm.class.php
+++ lib/form/custom/HeadPersonalForm.class.php
@@ -41,7 +41,7 @@ class HeadPersonalForm extends BasePersonalForm
 
   public function getFormFieldsDisplay()
   {
-    $personal_data_fields = array( 'person-lastname', 'person-firstname', 'person-identification_type', 'person-identification_number', 'person-sex', 'person-cuil', 'person-birthdate', 'person-birth_country', 'person-birth_state', 'person-birth_city', 'person-photo', 'person-observations' );
+    $personal_data_fields = array( 'person-lastname', 'person-firstname', 'person-identification_type', 'person-identification_number', 'person-sex', 'person-cuil', 'person-birthdate', 'person-birth_country', 'person-birth_state', 'person-birth_department', 'person-birth_city', 'person-photo', 'person-observations' );
     if($this->getObject()->getPerson()->getPhoto())
     {
       $personal_data_fields = array_merge($personal_data_fields, array('person-current_photo', 'person-delete_photo'));
