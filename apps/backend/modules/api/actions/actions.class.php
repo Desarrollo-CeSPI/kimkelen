2168dcddd0505810c065101d8afb4177bc2de4fc
Naimid <nai.videla@gmail.com>
2016-10-04 10:15:26 -0300

    pruebas para crear estudiante y tutor por post

--- apps/backend/modules/api/actions/actions.class.php
+++ apps/backend/modules/api/actions/actions.class.php
@@ -16,4 +16,34 @@ class apiActions extends sfActions
     $student = $this->getRoute()->getObject();
     $this->student = $student->asArray();
   }
+  
+  public function executeInsertStudent(sfWebRequest $request)
+  {
+	 $s_lastname = $request->getParameter('apellido');
+	 $s_firstname = $request->getParameter('nombres');
+	 $s_identification_type = $request->getParameter('tipo_documento_id'); //ver Ids con SIPECU
+	 $s_identification_number =$request->getParameter('nro_documento');
+	 $s_sex =$request->getParameter('student_sexo'); //ver Ids
+	 $s_phone =$request->getParameter('telefono_fijo');
+	 $s_birthdate =$request->getParameter('fecha_nacimiento');
+	 $s_birth_city =$request->getParameter('ciudad_nacimiento_id');
+	 
+	 $s_person = new Person();
+	 $s_person->setLastname($s_lastname);
+	 $s_person->setFirstname($s_firstname);
+	 $s_person->setIdentificationType($s_identification_type);
+	 $s_person->setIdentificationNumber($s_identification_number);
+	 $s_person->setSex($s_sex);
+	 $s_person->setPhone($s_phone);
+	 $s_person->setBirthdate($s_birthdate);
+	 $s_person->setIsActive(true);
+	 $s_person->setBirthCity($s_birth_city);
+	 
+	 $student= new Student();
+	 $student->setPerson($s_person);
+	 $student->setHealthCoverageId($request->getParameter('obra_social_id'));
+	 $student->setOriginSchoolId($request->getParameter('escuela_procedencia_numero'));
+	 
+	 die();
+  }
 }
