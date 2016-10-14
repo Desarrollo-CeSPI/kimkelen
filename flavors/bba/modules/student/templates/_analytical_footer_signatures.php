9f58756fb7c8ed70cd81adcf291184323216ee85
Ezequiel Gonzalez <egonzalez@cespi.unlp.edu.ar>
2016-10-14 12:10:03 -0300

    agrego los cambios en el analitico de BBA

---
+++ flavors/bba/modules/student/templates/_analytical_footer_signatures.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Kimkëlen - School Management Software
+ * Copyright (C) 2013 CeSPI - UNLP <desarrollo@cespi.unlp.edu.ar>
+ *
+ * This file is part of Kimkëlen.
+ *
+ * Kimkëlen is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v2.0 as published by
+ * the Free Software Foundation.
+ *
+ * Kimkëlen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kimkëlen.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
+ *
+ */
+?>
+<div id="analytic_signatures">
+    <div id="signature_1" class="signature"><?php echo __('analytic_signature_1'); ?></div>
+    <div id="signature_2" class="signature"><?php echo __('analytic_signature_1'); ?></div>
+    <div id="signature_3" class="signature"><?php echo __('analytic_signature_3'); ?></div>
+    <div id="signature_4" class="signature"><?php echo __('analytic_signature_2'); ?></div>
+</div>
