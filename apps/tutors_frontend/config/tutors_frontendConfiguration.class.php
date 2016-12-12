547622e5d111ecba5513dcf887af47efb0acc069
María Emilia Corrons <ecorrons@cespi.unlp.edu.ar>
2016-12-12 12:36:14 -0300

    cambios

--- apps/tutors_frontend/config/tutors_frontendConfiguration.class.php
+++ apps/tutors_frontend/config/tutors_frontendConfiguration.class.php
@@ -5,4 +5,19 @@ class tutors_frontendConfiguration extends sfApplicationConfiguration
   public function configure()
   {
   }
+
+
+  public function initialize()
+  {
+    parent::initialize();
+
+    // Register behavior's hooks
+
+
+    sfPropelBehavior::registerHooks('person_delete', array(
+      ':delete:pre' => array('PersonBehavior', 'deletePerson'),
+    ));
+
+
+  }
 }
