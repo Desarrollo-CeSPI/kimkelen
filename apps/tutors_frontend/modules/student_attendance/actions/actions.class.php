c3cfdc2b37103e80b46d5b4fdac2bca182e560b0
Naimid <nai.videla@gmail.com>
2016-12-29 09:14:59 -0300

    creacion de modulos

---
+++ apps/tutors_frontend/modules/student_attendance/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * student_attendance actions.
+ *
+ * @package    symfony
+ * @subpackage student_attendance
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class student_attendanceActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
---
+++ apps/tutors_frontend/modules/student_disciplinary_sanction/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * student_disciplinary_sanction actions.
+ *
+ * @package    symfony
+ * @subpackage student_disciplinary_sanction
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class student_disciplinary_sanctionActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
