d20ea6fc009f93cf923bc8127ce2c486f86cfa7c
Ezequiel González <egonzalez@cespi.unlp.edu.ar>
2017-02-15 11:05:04 -0300

    Cambios en la pantalla de estudiante y sus acciones (calificaciones, sanciones y asistencia)

--- apps/tutors_frontend/modules/califications/templates/showHistorySuccess.php
+++ apps/tutors_frontend/modules/califications/templates/showHistorySuccess.php
@@ -19,93 +19,101 @@
  */ ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 <div class="row">
-	<div class="col-md-1"></div>
-	<div class="col-md-10">
-		<div class="container-sombra-exterior container-califications">
+	<div class="col-md-12">
 
-			<div class="title-report-details title-califications" >
-	          	<span class="title-sanctions"><?php echo __('Califications report') . ' |'; ?></span> 
-	          	<span><?php echo $student  ?></span>
-	      	</div>
+        <?php include_partial('mainFrontend/personal_info', array('person' => $student)) ?>
 
-		 	<ul class="nav nav-tabs" role="tablist">
-				
-				<?php $last_year= $student->getLastStudentCareerSchoolYear()->getCareerSchoolYear()->getSchoolYear()->getYear();?>
-		 		<?php foreach ($student->getStudentCareerSchoolYears() as $student_career_school_year): ?>
-			  		<?php $year= $student_career_school_year->getCareerSchoolYear()->getSchoolYear()->getYear(); ?>	
-			  		<?php $class= ($student_career_school_year->getCareerSchoolYear()->getSchoolYear()->getIsActive() || ($year == $last_year))?'active' : ''; ?>
-			  		
-			  		
-			  		<li role="presentation"class="<?php echo $class?>" ><a href="<?php echo '#' . $year ?>" aria-controls="home" role="tab" data-toggle="tab"><?php echo $year ?></a></li>
-			 	<?php endforeach ?>                                                
-            </ul>
+        <div class="col-md-8">
+            <div class="row title-box">
+                <div class="col-md-12 title-icon">
+                    <?php echo image_tag("/frontend/images/notepad.svg", array('alt' => __('Califications'))); ?>
+                    <span class="title-text"> <?php echo __("Califications report");?> </span>
+                </div>
+            </div>
 
-                <div class="tab-content">
-                	<?php foreach ($student->getStudentCareerSchoolYears() as $student_career_school_year): ?>
-				    	<?php $year= $student_career_school_year->getCareerSchoolYear()->getSchoolYear()->getYear(); ?>
-			  			<?php $class= ($student_career_school_year->getCareerSchoolYear()->getSchoolYear()->getIsActive() || ($year == $last_year))?'tab-pane active' : 'tab-pane'; ?>
-			  	
-                            <div role="tabpanel" class="<?php echo $class ?>" id="<?php echo $year ?>"> 
+            <div class="row">
+                <div class="col-md-12 text-right">
+                    <?php echo button_to(__('Go back'), $link, array('class'=>'btn btn-default')) ?>
+                </div>
+            </div>
 
-                            	<?php $divisions = $student->getCurrentDivisions($student_career_school_year->getCareerSchoolYearId())?>
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <?php $last_year= $student->getLastStudentCareerSchoolYear()->getCareerSchoolYear()->getSchoolYear()->getYear();?>
+                        <?php foreach ($student->getStudentCareerSchoolYears() as $student_career_school_year): ?>
+                            <?php $year= $student_career_school_year->getCareerSchoolYear()->getSchoolYear()->getYear(); ?>
+                            <?php $class= ($student_career_school_year->getCareerSchoolYear()->getSchoolYear()->getIsActive() || ($year == $last_year))?'active' : ''; ?>
+                            <li role="presentation"class="<?php echo $class?>" ><a href="<?php echo '#' . $year ?>" aria-controls="home" role="tab" data-toggle="tab"><?php echo $year ?></a></li>
+                        <?php endforeach ?>
+                    </ul>
 
-							    <div class="status">
-							      <b><?php echo __('Year %%year%%',array('%%year%' => $student_career_school_year->getYear()))?></b>
-							      <b><?php echo __("Status") .': '?></b> <?php echo $student_career_school_year->getStatusString() ?>
-							    </div>
-						    
-						  		<?php $career_school_year = $student_career_school_year->getCareerSchoolYear(); ?>
-						  		<?php $course_subject_students = $student_career_school_year->getCourses(); ?>
-						  		<?php $career_student = CareerStudentPeer::retrieveByCareerAndStudent($career_school_year->getCareerId(), $student->getId()) ?>
-						  		<?php $back_url = isset($back_url) ? $back_url : '' ?>
+                    <div class="info-box">
+                        <?php foreach ($student->getStudentCareerSchoolYears() as $student_career_school_year): ?>
+                            <?php $year= $student_career_school_year->getCareerSchoolYear()->getSchoolYear()->getYear(); ?>
+                            <?php $class= ($student_career_school_year->getCareerSchoolYear()->getSchoolYear()->getIsActive() || ($year == $last_year))?'tab-pane active' : 'tab-pane'; ?>
 
-							  	<?php
-							  	isset($course_subject_students['ANUAL']) ? include_partial("califications/current_course_subjects", array("course_subject_students" => $course_subject_students['ANUAL'],
-							        'career_student' => $career_student,
-							        'back_url' => $back_url,
-							        'student' => $student,
-							        'course_type' => CourseType::TRIMESTER)) : ''
-							  	?>
+                            <div role="tabpanel" class="<?php echo $class ?>" id="<?php echo $year ?>">
 
-						  		<?php
-						  		isset($course_subject_students['QUATERLY']) ? include_partial("califications/current_course_subjects", array("course_subject_students" => $course_subject_students['QUATERLY'],
-						        	'career_student' => $career_student,
-						        	'back_url' => $back_url,
-						        	'student' => $student,
-						        	'course_type' => CourseType::QUATERLY)) : ''
-						  		?>
+                                <?php $divisions = $student->getCurrentDivisions($student_career_school_year->getCareerSchoolYearId())?>
 
-						  		<?php
-						  		isset($course_subject_students['BIMESTER']) ? include_partial("califications/current_course_subjects", array("course_subject_students" => $course_subject_students['BIMESTER'],
-						        	'career_student' => $career_student,
-						        	'back_url' => $back_url,
-						        	'student' => $student,
-						        	'course_type' => CourseType::BIMESTER)) : ''
-						  		?>
+                                <div class="status">
+                                    <b><?php echo __('Year %%year%%',array('%%year%' => $student_career_school_year->getYear()))?></b>
+                                    <b><?php echo __("Status") .': '?></b> <?php echo $student_career_school_year->getStatusString() ?>
+                                </div>
 
-						  		<?php
-						  		isset($course_subject_students['QUATERLY_OF_A_TERM']) ? include_partial("califications/current_course_subjects", array("course_subject_students" => $course_subject_students['QUATERLY_OF_A_TERM'],
-						    	    'career_student' => $career_student,
-						    	    'back_url' => $back_url,
-						    	    'student' => $student,
-						    	    'course_type' => CourseType::QUATERLY_OF_A_TERM)) : ''
-						  		?>
+                                <?php $career_school_year = $student_career_school_year->getCareerSchoolYear(); ?>
+                                <?php $course_subject_students = $student_career_school_year->getCourses(); ?>
+                                <?php $career_student = CareerStudentPeer::retrieveByCareerAndStudent($career_school_year->getCareerId(), $student->getId()) ?>
+                                <?php $back_url = isset($back_url) ? $back_url : '' ?>
 
-						  		<?php if ($anual_average = $student_career_school_year->getAnualAverage()): ?>
-						    		<div class="info_div">
-						     			<strong><?php echo __("Anual average") ?></strong> <em><?php echo $anual_average ?></em>
-						    		</div>
-								<?php endif; ?>	
+                                <?php
+                                isset($course_subject_students['ANUAL']) ? include_partial("califications/current_course_subjects", array("course_subject_students" => $course_subject_students['ANUAL'],
+                                    'career_student' => $career_student,
+                                    'back_url' => $back_url,
+                                    'student' => $student,
+                                    'course_type' => CourseType::TRIMESTER)) : ''
+                                ?>
+
+                                <?php
+                                isset($course_subject_students['QUATERLY']) ? include_partial("califications/current_course_subjects", array("course_subject_students" => $course_subject_students['QUATERLY'],
+                                    'career_student' => $career_student,
+                                    'back_url' => $back_url,
+                                    'student' => $student,
+                                    'course_type' => CourseType::QUATERLY)) : ''
+                                ?>
+
+                                <?php
+                                isset($course_subject_students['BIMESTER']) ? include_partial("califications/current_course_subjects", array("course_subject_students" => $course_subject_students['BIMESTER'],
+                                    'career_student' => $career_student,
+                                    'back_url' => $back_url,
+                                    'student' => $student,
+                                    'course_type' => CourseType::BIMESTER)) : ''
+                                ?>
+
+                                <?php
+                                isset($course_subject_students['QUATERLY_OF_A_TERM']) ? include_partial("califications/current_course_subjects", array("course_subject_students" => $course_subject_students['QUATERLY_OF_A_TERM'],
+                                    'career_student' => $career_student,
+                                    'back_url' => $back_url,
+                                    'student' => $student,
+                                    'course_type' => CourseType::QUATERLY_OF_A_TERM)) : ''
+                                ?>
+
+                                <?php if ($anual_average = $student_career_school_year->getAnualAverage()): ?>
+                                    <div class="info_div">
+                                        <strong><?php echo __("Anual average") ?></strong> <em><?php echo $anual_average ?></em>
+                                    </div>
+                                <?php endif; ?>
 
                             </div>
-                                        
-                	<?php endforeach ?>
+
+                        <?php endforeach ?>
+
+                    </div>
                 </div>
-		</div>	    
+            </div>
+        </div>
 	</div>
-	<div class="col-md-1"></div>
-	<div class="col-md-12 container-buttons">
-    	<?php echo button_to(__('Go back'), $link, array('class'=>'btn btn-default')) ?>
-  	</div>
 </div>
--- apps/tutors_frontend/modules/student_attendance/templates/indexSuccess.php
+++ apps/tutors_frontend/modules/student_attendance/templates/indexSuccess.php
@@ -18,26 +18,47 @@
  * along with Kimkëlen.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
  */ ?>
 
- <div class="row"> 
- 	<div class="col-md-1"></div>
- 	<div class="col-md-10 container-sombra-exterior container-attendance">
- 		<div class="box-title">		
+ <div class="row">
+ 	<div class="col-md-12">
+
+        <?php include_partial('mainFrontend/personal_info', array('person' => $student)) ?>
+
+        <div class="col-md-8">
+            <div class="row title-box">
+                <div class="col-md-12 title-icon">
+                    <?php echo image_tag("/frontend/images/attendance.svg", array('alt' => __('Attendance'))); ?>
+                    <span class="title-text"> <?php echo __("Attendance");?> </span>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12 text-right">
+                    <?php echo button_to(__('Go back'), $link, array('class'=>'btn btn-default')) ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12 info-box">
+                    <?php if(! $student->hasAttendancesPerSubject()): ?>
+                        <?php include_partial('attendance_per_day', array('student_career_school_year'=>$student_career_school_year,'division'=>$division,'student'=>$student)); ?>
+                    <?php else: ?>
+                        <?php include_partial('attendance_per_subject', array('student_career_school_year'=>$student_career_school_year,'student' => $student)); ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+
+        </div>
+ 	</div>
+ </div>
+
+
+<!--
+ 		<div class="box-title">
 			<span class="title-sanctions"><?php echo __('Attendance'); ?> |</span>
 			<span class=""><?php echo $student . ' - ' . __('school year') . ' '. $school_year->getYear()?></span>
 		</div>
- 		<?php if(! $student->hasAttendancesPerSubject()): ?>
 
- 			<?php include_partial('attendance_per_day', array('student_career_school_year'=>$student_career_school_year,'division'=>$division,'student'=>$student)); ?>
- 			
- 		<?php else: ?>
-
- 			<?php include_partial('attendance_per_subject', array('student_career_school_year'=>$student_career_school_year,'student' => $student)); ?>
-
-	 	<?php endif; ?>
 	<div class="col-md-12 container-buttons">
-		<?php echo link_to(__('Go back'), $link, array("class"=> "button_1"));?>
-		<?php echo link_to(__('Show report'), 'student_attendance/showReport?student_id=' . $student->getId(), array("class"=> "button_2"));?>		
+		<?php echo link_to(__('Show report'), 'student_attendance/showReport?student_id=' . $student->getId(), array("class"=> "button_2"));?>
 	</div>
- 	</div>
- 	<div class="col-md-1"></div>
- </div>
+-->
