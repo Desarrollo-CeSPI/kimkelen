901d39d691bb8ab64b29dc1054a5af64029cae59
Ezequiel González <egonzalez@cespi.unlp.edu.ar>
2017-02-16 13:57:24 -0300

    fix llamado a un die()

--- apps/tutors_frontend/modules/califications/templates/_current_course_subjects.php
+++ apps/tutors_frontend/modules/califications/templates/_current_course_subjects.php
@@ -21,15 +21,7 @@
 <?php unset($course_subject_students['marks']); ?>
 <?php unset($course_subject_students['periods']); ?>
 
-<?php
-
-die(var_dump(SchoolBehaviourFactory::getEvaluatorInstance()));
-$evaluator_instance = SchoolBehaviourFactory::getEvaluatorInstance();
-
-
-
-?>
-
+<?php $evaluator_instance = SchoolBehaviourFactory::getEvaluatorInstance(); ?>
 
 <?php $columns = 6 + $marks + count($evaluator_instance->getExaminationNumbers()) ?>
 
