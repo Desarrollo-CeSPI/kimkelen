17c3d35d8f37f620f42914cb282979b62a4a3e7a
María Emilia Corrons <ecorrons@cespi.unlp.edu.ar>
2017-02-24 16:54:47 -0300

    dejo codigo tal cual estaba en master en lugar de en rama bis

--- flavors/lvm/lib/behaviour/LvmEvaluatorBehaviour.class.php
+++ flavors/lvm/lib/behaviour/LvmEvaluatorBehaviour.class.php
@@ -400,17 +400,7 @@ class LvmEvaluatorBehaviour extends BaseEvaluatorBehaviour
 
       if ($student_repproved_course_subject = StudentRepprovedCourseSubjectPeer::retrieveByCourseSubjectStudent($course_subject_student))
       {
-        $sers = $student_repproved_course_subject->getStudentExaminationRepprovedSubjects();
-          //$sers = StudentExaminationRepprovedSubjectPeer::retrieveByStudentRepprovedCourseSubject($student_repproved_course_subject);
-
-          if ($sers > 1) 
-          {
-            foreach ($sers as $student_examination_repproved_subject) 
-            {
-              $student_examination_repproved_subject->delete($con);
-            }
-          }
-          $student_repproved_course_subject->delete($con);
+        $student_repproved_course_subject->delete($con);
       }
     }
     else
