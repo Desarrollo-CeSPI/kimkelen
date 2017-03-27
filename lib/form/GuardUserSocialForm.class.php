17aeeedfc25c5b14acf98ee322a3bec577694798
Naimid <nai.videla@gmail.com>
2017-03-27 12:13:54 -0300

    cambio de clave en primer login

---
+++ lib/form/GuardUserSocialForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * GuardUserSocial form.
+ *
+ * @package    symfony
+ * @subpackage form
+ * @author     Your name here
+ */
+class GuardUserSocialForm extends BaseGuardUserSocialForm
+{
+  public function configure()
+  {
+  }
+}
