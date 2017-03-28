92b34acd147d511b2d3eee44b8192d1b7765e739
María Emilia Corrons <ecorrons@cespi.unlp.edu.ar>
2017-03-28 13:29:38 -0300

    nuevo parche

---
+++ patch/2.26.0/patch.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `guard_user_social` (`id` INTEGER  NOT NULL AUTO_INCREMENT, `social_id` VARCHAR(255)  NOT NULL, `user_id` INTEGER, PRIMARY KEY (`id`), INDEX `guard_user_social_FI_1` (`user_id`), CONSTRAINT `guard_user_social_FK_1` FOREIGN KEY (`user_id`) REFERENCES `sf_guard_user` (`id`) ON DELETE CASCADE )Engine=InnoDB;
