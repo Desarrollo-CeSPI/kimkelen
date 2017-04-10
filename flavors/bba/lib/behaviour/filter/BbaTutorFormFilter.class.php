2221326defa38088736f8e4e8e7070145494b43e
Naimid <nai.videla@gmail.com>
2017-04-10 12:39:58 -0300

    filtro de tutores por año de cursada del alumno

---
+++ flavors/bba/lib/behaviour/filter/BbaTutorFormFilter.class.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Tutor filter form.
+ *
+ * @package    conservatorio
+ * @subpackage filter
+ * @author     Desarrollo CeSPI
+ * @version    SVN: $Id: sfPropelFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
+ */
+class BbaTutorFormFilter extends TutorFormFilter
+{
+
+  public function configure()
+  {
+      
+    parent::configure();
+    
+    $c_criteria = new Criteria(CareerPeer::DATABASE_NAME);
+    
+    $this->setWidget('career', new sfWidgetFormPropelChoice(array('model' => 'Career', 'criteria' => $c_criteria, 'add_empty' => true)));
+    $this->setValidator('career', new sfValidatorPropelChoice(array('model' => 'Career', 'criteria' => $c_criteria, 'required' => false)));
+	
+    $w = new sfWidgetFormChoice(array('choices' => array()));
+    
+    $this->setWidget('year', new dcWidgetAjaxDependence(array(
+        'dependant_widget' => $w,
+        'observe_widget_id' => 'tutor_filters_career',
+        'message_with_no_value' => 'Seleccione una carrera',
+        'get_observed_value_callback' => array(get_class($this), 'getYears')
+      )));
+    
+    $max = CareerPeer::getMaxYear();
+    $years = array('' => '');
+    for ($i = 1; $i <= $max; $i++)
+      $years[$i] = $i;
+    
+    $this->setValidator('year', new sfValidatorChoice(array('choices' => array_keys($years), 'required' => false)));
+      
+    $this->getWidgetSchema()->setHelp('year', 'El año filtra de acuerdo al año en el que se encuentra cursando el alumno.');
+    
+  }
+
+  public function getFields()
+  {
+    return array_merge(parent::getFields(), array('career' => 'Number', 'year' => 'Number'));
+
+  }
+  
+  public static function getYears($widget, $values){
+	
+	$career = CareerPeer::retrievebyPk($values);
+	$max = $career->getMaxYear();
+
+	$years = array('' => '');
+    for ($i = 1; $i <= $max; $i++)
+      $years[$i] = $i;
+    $widget->setOption('choices', $years);
+  }
+  
+  public function addCareerColumnCriteria(Criteria $criteria , $field, $values)
+  {
+    if ($values)
+    {
+      $criteria->add(CareerStudentPeer::CAREER_ID, $values);
+      $criteria->addJoin(CareerStudentPeer::STUDENT_ID, StudentPeer::ID);
+      $criteria->addJoin(StudentPeer::ID, StudentTutorPeer::STUDENT_ID);
+      $criteria->addJoin(StudentTutorPeer::TUTOR_ID, TutorPeer::ID);
+    }
+  }
+  
+  public function addYearColumnCriteria(Criteria $criteria , $field, $values)
+  {
+    if ($values)
+    {
+      $criteria->add(StudentCareerSchoolYearPeer::YEAR, $values);
+      $criteria->addJoin(StudentCareerSchoolYearPeer::STUDENT_ID, StudentPeer::ID);
+      $criteria->addJoin(StudentPeer::ID, StudentTutorPeer::STUDENT_ID);
+      $criteria->addJoin(StudentTutorPeer::TUTOR_ID, TutorPeer::ID);
+      $criteria->addJoin(StudentCareerSchoolYearPeer::CAREER_SCHOOL_YEAR_ID, CareerSchoolYearPeer::ID);
+      $criteria->addJoin(CareerSchoolYearPeer::CAREER_ID, CareerPeer::ID);
+      $criteria->add(CareerSchoolYearPeer::SCHOOL_YEAR_ID, SchoolYearPeer::retrieveCurrent()->getId());
+    }
+  }
+/*
+ 
+
+  public function addDivisionIdColumnCriteria(Criteria $criteria, $field, $value)
+  {
+    if ($value)
+    {
+      $criteria->add(DivisionPeer::ID, $value);
+      $criteria->addJoin(DivisionPeer::ID, DivisionStudentPeer::DIVISION_ID);
+      $criteria->addJoin(StudentPeer::ID, DivisionStudentPeer::STUDENT_ID);
+      $criteria->addJoin(StudentPeer::ID, StudentTutorPeer::STUDENT_ID);
+      $criteria->addJoin(StudentTutorPeer::TUTOR_ID, TutorPeer::ID);
+    }
+
+  }
+*/
+}
