de62799efbabb47f9022e2a6f807c3270b8f3fef
Naimid Videla <nai.videla@gmail.com>
2017-11-07 12:44:29 -0300

    modifico texto en header de analitico lvm

--- flavors/lvm/modules/student/templates/_analytical_header_text.php
+++ flavors/lvm/modules/student/templates/_analytical_header_text.php
@@ -27,6 +27,7 @@
         nacido/a en <span><?php echo ucwords($student->getPerson()->getBirthCityRepresentation()); ?>, <?php echo ucwords($student->getPerson()->getBirthStaterepresentation()); ?>, <?php echo $student->getPerson()->getBirthCountryRepresentation() ?></span>,
         el día <strong><?php echo format_date($student->getPerson()->getBirthDate(), "D") ?></strong>,
         que ingresó en este establecimiento en el año <span><?php echo $student->getInitialSchoolYear()->getYear(); ?></span>
-        proveniente de <span><?php echo ($student->getOriginSchool()? $student->getOriginSchool():__('otra escuela')); ?></span> donde finalizó sus estudios de <?php echo __('nombre_ultimo_anio_primario'); ?> y aprobó en este Establecimiento las asignaturas que con sus respectivas notas se expresan:
+        proveniente de <span><?php echo ($student->getOriginSchool()? $student->getOriginSchool():__('otra escuela')); ?></span> donde finalizó sus estudios de <?php $initial_scsy = $student->getCareerYear(CareerSchoolYearPeer::retrieveByCareerAndSchoolYear($student->getCareerStudent()->getCareer(), $student->getInitialSchoolYear()));?>
+        <?php echo ($initial_scsy > 1 )? __($initial_scsy -1) . '° año de la ESB': __('nombre_ultimo_anio_primario'); ?>  y aprobó en este Establecimiento las asignaturas que con sus respectivas notas se expresan:
     </p>
 </div>
