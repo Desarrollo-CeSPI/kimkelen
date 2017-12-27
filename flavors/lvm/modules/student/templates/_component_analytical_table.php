3306660b955b4cf726c96234f9d9b6155660d3a6
Naimid Videla <nai.videla@gmail.com>
2017-12-27 12:55:38 -0300

    promedio general con dos decimales

--- flavors/lvm/modules/student/templates/_component_analytical_table.php
+++ flavors/lvm/modules/student/templates/_component_analytical_table.php
@@ -66,7 +66,7 @@
   <?php endforeach ?>
 
   <?php if ($object->has_completed_career()): ?>
-    <div id="promedio_gral"><?php echo __('Promedio general'); ?>: <span id="promedio_gral_valor"><?php echo ($object->get_total_average()?round($object->get_total_average(),2):'-'); ?></span></div>
+    <div id="promedio_gral"><?php echo __('Promedio general'); ?>: <span id="promedio_gral_valor"><?php echo ($object->get_total_average()? number_format(round($object->get_total_average(), 2), 2, '.', '') :'-'); ?></span></div>
   <?php endif; ?>
 <?php endif; ?>
 
