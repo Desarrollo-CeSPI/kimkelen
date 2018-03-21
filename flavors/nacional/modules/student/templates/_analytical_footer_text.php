e6316bba3737145f45f1bee0ad6bf505e80c9189
Naimid Videla <nai.videla@gmail.com>
2018-03-21 08:32:56 -0300

    nuevos cambios para analitico del nacional

--- flavors/nacional/modules/student/templates/_analytical_footer_text.php
+++ flavors/nacional/modules/student/templates/_analytical_footer_text.php
@@ -21,16 +21,16 @@
 <?php use_helper('Date') ?>
 
     <div class="header-text">
-        <p>
+        <div class="footer-text">
         <?php if($student->getOriginSchool()):?>
           Certificado de Educación Primaria expedido por: <?php echo BaseCustomOptionsHolder::getInstance('SectorOriginSchoolType')->getStringFor($student->getOriginSchool()->getSector()) ?>  
           <?php echo ($student->getOriginSchool()->getSector() != SectorOriginSchoolType::SECTOR_UNLP)? ' - Provincia de ' .  $student->getOriginSchool()->getCity()->getDepartment()->getState() : ''?>
         <?php endif ?> 
-        </p>
+        </div>
         <?php if ($analytical->has_completed_career()): ?>
-            <p>Completó sus estudios secundarios. <?php echo $analytical->get_career_student()->getCareer()->getCareerName(); ?>. </p>
+            <div class="footer-text">Completó sus estudios secundarios. <?php echo $analytical->get_career_student()->getCareer()->getCareerName(); ?>. </div>
         <?php else: ?>
-            <p>Para terminar sus estudios secundarios deberá aprobar:
+            <div class="footer-text">Para terminar sus estudios secundarios deberá aprobar:
             <?php if ($analytical->has_missing_subjects() ): ?>
                 <?php $missing_subjects = $analytical->get_missing_subjects(); end($missing_subjects); $last_key = key($missing_subjects);?>
                 <?php foreach ($analytical->get_missing_subjects() as $key => $subject): ?> <?php if (0!=$key): if ($key === $last_key): ?>y<?php else: ?>,<?php endif;endif; ?>
@@ -40,9 +40,9 @@
             <?php if ($analytical->has_remaining_years() ): ?>
                 Todo <?php echo implode(', ', array_map('__', $analytical->get_remaining_years_string())); ?>.
             <?php endif; ?>
-            </p>
-	    <p>Certificado de Estudios Incompleto.</p>
+            </div>
+            <div class="footer-text">Certificado de Estudios Incompleto.</div>
         <?php endif; ?>
-        <p>Para que conste y a pedido del interesado, se expide el presente certificado confrontado con los registros y actas originales por el <?php echo __('Departamento de alumnos'); ?>, en la ciudad de <?php echo __('escuela_ciudad'); ?>, a los <?php echo date('d'); ?> días del mes de <?php echo format_date(time(), 'MMMM'); ?> de <?php echo date('Y'); ?>.</p>
+            <div class="footer-text">Para que conste y a pedido del interesado, se expide el presente certificado confrontado con los registros y actas originales por el <?php echo __('Departamento de alumnos'); ?>, en la ciudad de <?php echo __('escuela_ciudad'); ?>, a los <?php echo date('d'); ?> días del mes de <?php echo format_date(time(), 'MMMM'); ?> de <?php echo date('Y'); ?>.</div>
 
     </div>
