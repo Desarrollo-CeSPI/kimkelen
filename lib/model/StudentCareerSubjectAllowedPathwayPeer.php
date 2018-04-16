bd844e3d7c99efa89d3b8084767f2ced90dfcd8e
Naimid Videla <nai.videla@gmail.com>
2018-04-16 12:40:10 -0300

    fix en calculo de materias de trayectorias

--- lib/model/StudentCareerSubjectAllowedPathwayPeer.php
+++ lib/model/StudentCareerSubjectAllowedPathwayPeer.php
@@ -2,4 +2,17 @@
 
 class StudentCareerSubjectAllowedPathwayPeer extends BaseStudentCareerSubjectAllowedPathwayPeer
 {
+    static public function retrieveCriteriaByStudentAndCareerSubject($student, $career_subject)
+    {
+        $c = new Criteria();
+        $c->add(self::STUDENT_ID, $student->getId());
+        $c->add(self::CAREER_SUBJECT_ID, $career_subject->getId());
+
+        return $c;
+    }
+    
+    static public function doCountStudentAndCareerSubject($student, $career_subject)
+    {
+            return self::doCount(self::retrieveCriteriaByStudentAndCareerSubject($student, $career_subject));
+    }
 }
