89832eaa22565f728cb89f0359e40fb6292d9701
Naimid Videla <nai.videla@gmail.com>
2018-09-04 13:18:13 -0300

    modificaciones nacional

--- flavors/nacional/lib/behavior/factory/NacionalFormFactory.class.php
+++ flavors/nacional/lib/behavior/factory/NacionalFormFactory.class.php
@@ -33,5 +33,10 @@ class NacionalFormFactory extends BaseFormFactory {
   {
       return 'NacionalAnalyticForm';    
   }
+  
+  public function getStudentForm()
+  {
+      return 'NacionalStudentForm';    
+  }
 
 }
---
+++ patch/2.33.0/patch.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `student` 
+ADD COLUMN `judicial_restriction` TINYINT(4) NULL DEFAULT 0 AFTER `origin_school_id`;
+
