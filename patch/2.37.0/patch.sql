c683003726d4caaacca285b5a7d1bddaee303670
Naimid Videla <nai.videla@gmail.com>
2019-06-06 10:58:00 -0300

    parches

---
+++ patch/2.37.0/patch.sql
@@ -0,0 +1,4 @@
+insert into sf_guard_permission (name, description) values ('print_report_card','Imprimir boletines y analíticos');
+insert into sf_guard_permission (name, description) values ('print_certificate','Imprimir constancias y certificados');
+insert into sf_guard_permission (name, description) values ('show_assistance_sanction_report','Ver reporte de inasistencias y sanciones');
+insert into sf_guard_permission (name, description) values ('show_division_califications','Ver las calificaciones de una división');
