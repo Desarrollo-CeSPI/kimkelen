0280801b9e3a8fb3bad755b83d2dbd62736250a0
Naimid Videla <nai.videla@gmail.com>
2019-06-12 10:59:06 -0300

    correciones de permisos en preceptores

--- patch/2.37.1/patch.sql
+++ patch/2.37.1/patch.sql
@@ -1,1 +1,9 @@
 insert into sf_guard_permission (name, description) values ('edit_conduct','Editar notas de conducta');
+update sf_guard_permission set  description = 'Imprimir boletines' where name = 'print_report_card';
+insert into sf_guard_permission (name, description) values ('print_analytic','Imprimir analíticos');
+insert into sf_guard_permission (name, description) values ('print_graduate_certificate','Imprimir constancias de egresados');
+insert into sf_guard_permission (name, description) values ('print_regular_certificate','Imprimir constancias de alumno regular');
+insert into sf_guard_permission (name, description) values ('print_free_certificate','Imprimir constancias de alumno libre');
+insert into sf_guard_permission (name, description) values ('print_withdrawn_certificate','Imprimir constancias de alumno retirado');
+
+
