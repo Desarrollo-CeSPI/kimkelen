2f1d7e3b556b2abfe3475d3380f0be48524ad1d5
Naimid Videla <nai.videla@gmail.com>
2019-07-01 14:03:49 -0300

    modifico boletines lvm para no promediar en retirados

--- flavors/lvm/modules/report_card/templates/_course_subject_bimester.php
+++ flavors/lvm/modules/report_card/templates/_course_subject_bimester.php
@@ -40,7 +40,7 @@
 
       <?php $course_result = $course_subject_student->getCourseResult() ?>
       <?php if (!$course_subject_student->hasSomeMarkFree()): ?>
-        <td><?php echo ($course_result) ? $course_result->getResultStr() : '' ?></td>
+        <td><?php echo ($course_result && $course_subject_student->getAllMarksWithNote()) ? $course_result->getResultStr() : '' ?></td>
       <?php else: ?>
         <td></td>
       <?php endif; ?>
@@ -112,7 +112,7 @@
 
       <?php $course_result = $course_subject_student->getCourseResult() ?>
       <?php if (!$course_subject_student->hasSomeMarkFree()): ?>
-        <td><?php echo ($course_result) ? $course_result->getResultStr() : '' ?></td>
+        <td><?php echo ($course_result && $course_subject_student->getAllMarksWithNote()) ? $course_result->getResultStr() : '' ?></td>
       <?php else: ?>
         <td></td>
       <?php endif; ?>
