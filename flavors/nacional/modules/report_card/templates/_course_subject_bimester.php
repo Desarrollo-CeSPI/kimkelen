975436c760455497139eec7901c5838ecde5dca6
Naimid Videla <nai.videla@gmail.com>
2019-08-16 12:13:16 -0300

    modificaciones para mostrar inasistencias y sanciones

--- flavors/nacional/modules/report_card/templates/_course_subject_bimester.php
+++ flavors/nacional/modules/report_card/templates/_course_subject_bimester.php
@@ -65,7 +65,7 @@
         <?php foreach ($periods[0] as $period): ?>
           <td>
             <?php
-            echo $period->getIsClosed() ? round($student->getTotalAbsences($division->getCareerSchoolYearId(), $period, $course_subject_student->getCourseSubjectId(), true), 2) : '&nbsp'
+            echo round($student->getTotalAbsences($division->getCareerSchoolYearId(), $period, $course_subject_student->getCourseSubjectId(), true), 2)
             ?>
           </td>
         <?php endforeach; ?>
@@ -123,7 +123,7 @@
         <?php foreach ($periods[1] as $period): ?>
           <td>
             <?php
-            echo $period->getIsClosed() ? round($student->getTotalAbsences($division->getCareerSchoolYearId(), $period, $course_subject_student->getCourseSubjectId(), true), 2) : '&nbsp'
+            echo round($student->getTotalAbsences($division->getCareerSchoolYearId(), $period, $course_subject_student->getCourseSubjectId(), true), 2)
             ?>
           </td>
           <?php endforeach; ?>
