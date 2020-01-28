746766ee07ca7b4626ad55c320730ec55921e28c
Naimid Videla <nai.videla@gmail.com>
2020-01-28 10:36:41 -0300

    modificaciones en actas de examen

--- apps/backend/modules/examination_subject/templates/printRecordSuccess.php
+++ apps/backend/modules/examination_subject/templates/printRecordSuccess.php
@@ -19,136 +19,14 @@
 <?php use_stylesheet('examination-record.css') ?>
 <?php use_stylesheet('print-examination-record.css', 'last', array('media' => 'print')) ?>
 
-<?php $evaluator_instance = SchoolBehaviourFactory::getEvaluatorInstance();?>
 <?php foreach ($record->getRecordSheets() as $rs):?>
 <div class="record-wrapper">
-  <div class="record-header">
-      <div class="logo"><?php echo image_tag("kimkelen_logo.png", array('absolute' => true,'class' => 'logo_print_record')) ?></div>
-  </div>  
-  <div style="text-align: center">
-     <h2>Acta de Examen</h2>
-  </div>
-  <div class="white-background"><?php echo 'Exámenes de Alumnos: ' . $examination_subject->getExamination() ; ?></div>
-  <div class="gray-background">
-      <strong><?php echo __('Subject'); ?></strong>:
-      <strong><?php echo $examination_subject->getSubject() . ' - ' . $examination_subject->getYear() . ' año'  ?></strong>
-      
-      <span class="right">
-          <strong><?php echo __('School year'); ?></strong>: <?php echo $examination_subject->getExamination()->getSchoolYear() ?> 
-          <strong> <?php echo __('Day')?>:</strong><?php echo  (!is_null($examination_subject->getDate())) ? ' ' . date_format(new DateTime($examination_subject->getDate()), "d/m/Y") : ' _____ / _____ / _____' ?>  &nbsp; </span>
-  </div>
-  
-  <div class="gray-background">
-    <span><strong><?php echo 'Acta N°: '; ?></strong>  <?php echo $record->getId(); ?> </span>
-    <span class="right"><strong><?php echo 'Tomo: '; ?></strong><?php echo ($rs->getBook()) ? $rs->getBook() : ' _______________________ '; ?>     <strong> <?php echo 'Folio físico: '; ?></strong><?php echo ($rs->getPhysicalSheet())? $rs->getPhysicalSheet() : ' ________ '; ?></span>
-  </div>
-  <table class="gridtable_bordered">
-    <thead>
-      <tr class="printColumns">
-        <th class="orden" rowspan="2"><?php echo __('N°'); ?> </th>
-        <th rowspan="2"><?php echo __('Apellido y Nombre'); ?></th>
-        <th class="division_record" rowspan="2"><?php echo __('Division'); ?></th>
-        <th colspan="2"><?php echo __('Mark'); ?></th>
-        <th class="result_record" rowspan="2"><?php echo __('Resultado'); ?></th>
-      </tr>
-      <tr>
-        <th class="calification_number" colspan="1"><?php echo __('Números'); ?></th>
-        <th class="calification_letter" colspan="1"><?php echo __('Letras'); ?></th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php $i = 1; ?>
-      <?php foreach ($record->getRecordDetailsForSheet($rs->getSheet()) as $rd): ?>
-        <tr>
-          <td><?php echo $rd->getLine() ?> </td>
-          <td class="student"><?php echo $rd->getStudent() ?> <?php if($rd->getOwesCorrelative()): ?> <span class="owes_correlative"><?php echo "(". __('Owes correlative') . ")" ?></span> <?php endif; ?></td>
-          <td> <?php echo $rd->getDivision() ?> </td>
-          <td><?php echo ($rd->getMark())? $rd->getMark(): ''; ?></td>
-          <td>
-            <?php $c = new num2text();?>
-            <?php if(!$rd->getIsAbsent()):?>
-               <?php echo $c->num2str($rd->getMark()) ?>
-              <?php else: ?>
-                <?php echo __('Absent'); ?>
-              <?php endif; ?>
-          </td>
-          <td> <?php echo $evaluator_instance->getResultStringFor($rd->getResult()) ?></td>
-        </tr>
-      <?php endforeach; ?>
-    </tbody>
-  </table>
-  <div class="article-div">
-    <strong>Art 34º: </strong><span class="sub">Exámenes regulares:</span> La evaluación en las mesas de exámenes regulares, regulares complementarios o regulares previos, se realizará sobre aquellos contenidos desarrollados durante el ciclo lectivo cursado.
-  </div>
+   
+  <?php include_partial('examination_subject/header_record',array('examination_subject'=>$examination_subject, 'record'=>$record, 'rs' => $rs)); ?>
 
-  <div class="article-div">
-    <strong>Art 35º: </strong><span class="sub">Exámenes libres:</span> La evaluación en las mesas de exámenes de alumnos libres, se realizará sobre aquellos contenidos del programa del ciclo lectivo cursado de acuerdo a las reglamentaciones vigentes.
-  </div>
+  <?php include_partial('examination_subject/body_record',array( 'record'=>$record, 'rs' => $rs)); ?>
 
-  <div class="article-div">
-    <strong>En ambos casos: </strong>"La evaluación podrá ser oral y/o escrita y/o práctica. Siempre que se utilice más de una modalidad, éstas no podrán ser eliminatorias entre sí, debiendo tener un carácter complementario.
-  </div>
-  <div class="article-div">
-    <div class="observation-box">
-      <strong><?php echo __('Observations'); ?></strong>:
-    </div>
-  </div>
-  <div class="article-div">
-    <p>La Mesa Examinadora para la evaluación de los alumnos inscriptos en la presente acta, ha utilizado la modalidad (marcar lo que corresponda)</p>
-    <div>
-      <span><?php echo __('Oral'); ?>:</span>
-      <span class="little-box"></span>
-      <span><?php echo __('Escrita'); ?>:</span>
-      <span class="little-box"></span>
-      <span><?php echo __('Práctica'); ?>:</span>
-      <span class="little-box"></span>
-    </div>
-  </div>
-
-    <div class="article-div">
-      <strong><?php echo __('Total de alumnos'); ?>:</strong>
-      <span class="little-box">
-        <?php echo $record->countRecordDetailsForSheet($rs->getSheet()) ?>
-      </span>
-
-      <strong><?php echo __('Aprobados'); ?>:</strong>
-      <span class="little-box">
-        <?php echo $record->countRecordDetailsForSheetAndResult($rs->getSheet(),$evaluator_instance->getApprovedResult()) ?>
-      </span>
-
-      <strong><?php echo __('Aplazados'); ?>:</strong>
-      <span class="little-box">
-        <?php echo $record->countRecordDetailsForSheetAndResult($rs->getSheet(),$evaluator_instance->getDisapprovedResult()) ?>
-      </span>
-
-      <strong><?php echo __('Ausentes'); ?>:</strong>
-      <span class="little-box">
-        <?php echo $record->countRecordDetailsForSheetAndResult($rs->getSheet(),$evaluator_instance->getAbsentResult()) ?>
-      </span>
-    </div>
-
-  <div class="record-footer">
-      <div class="article-div">
-        La Plata, __________ de ______________________ de __________
-      </div>
-      <div style="margin-left: 25px;" class="signature">
-        <p class="signature-text">Vocal</p>
-        <p class="signature-subtext">Firma y aclaración</p>
-      </div>
-      <div class="signature">
-        <p class="signature-text">Presidente</p>
-        <p class="signature-subtext">Firma y aclaración</p>
-      </div>
-      <div class="signature">
-        <p class="signature-text">Vocal</p>
-        <p class="signature-subtext">Firma y aclaración</p>
-      </div>
-      <div class="sheet-record">
-          <span>
-              Hoja <?php echo $rs->getSheet() . '/' . count($record->getRecordSheets())?>
-          </span>
-      </div>
-  </div>
+  <?php include_partial('examination_subject/footer_record',array( 'record'=>$record, 'rs' => $rs)); ?>
 
 </div>
 <div style="page-break-before: always;"></div>
