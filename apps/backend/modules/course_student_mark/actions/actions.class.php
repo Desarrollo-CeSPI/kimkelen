2a6b23dae2948878f6edd240e59a144cbc6ec688
Naimid <nai.videla@gmail.com>
2021-02-10 10:41:26 -0300

    cambios

--- apps/backend/modules/course_student_mark/actions/actions.class.php
+++ apps/backend/modules/course_student_mark/actions/actions.class.php
@@ -371,26 +371,6 @@ class course_student_markActions extends sfActions
       
   }
   
-  
-  protected function getForms($course_subjects, $is_pathway)
-  {
-    $forms = array();
-
-    foreach ($course_subjects as $course_subject)
-    {
-      
-      if ($is_pathway){
-	      $form_name = SchoolBehaviourFactory::getInstance()->getFormFactory()->getCourseSubjectPathwayMarksForm();
-      } else {
-	      $form_name = SchoolBehaviourFactory::getInstance()->getFormFactory()->getCourseSubjectMarksForm();
-      }
-      $forms[$course_subject->getId()] = new $form_name($course_subject);
-    }
-
-    return $forms;
-
-  }
-  
   public function executeNotAverageableCalifications(sfWebRequest $request)
   {
     $this->course = $this->getCourse();
