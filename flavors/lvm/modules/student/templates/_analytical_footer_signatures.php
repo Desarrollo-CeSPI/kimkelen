c76f5adf66b91cea044a256ec7e0b3f749049e14
Naimid Videla <nai.videla@gmail.com>
2022-05-25 17:07:11 -0300

    cambio footer analitico lvm

--- flavors/lvm/modules/student/templates/_analytical_footer_signatures.php
+++ flavors/lvm/modules/student/templates/_analytical_footer_signatures.php
@@ -21,11 +21,11 @@
 ?>
 <div id="analytic_signatures">
     <div id="signature_1" class="signature">
-        <div> Prof. María Julia Alba</div>
+        <div> Prof. María Constanza Erbetta</div>
         <?php echo __('analytic_signature_1'); ?>
     </div>
     <div id="signature_2" class="signature">
-        <div>Prof. María Constanza Erbetta</div>
+        <div>Prof. Julieta Miranda</div>
         <?php echo __('analytic_signature_2'); ?>
     </div>
     <div id="signature_4" class="signature signature_4">
